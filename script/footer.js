cdc43f5c35d59b38d809325ed715133d8928fa1c
Mike Powell <mike@lakeslegendaries.com>
2022-03-10 15:11:32 -0500

    empire video page

---
+++ script/footer.js
@@ -0,0 +1,5 @@
+// see button thumbnails
+$('#video_button').click(function(){
+    document.getElementById("video_button").classList.add("clicked");
+    document.getElementById("video_thumbnails").style.display = "block";
+})
