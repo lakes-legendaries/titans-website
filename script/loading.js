41cdca286191dfe9f1dc59e9ef63e6b01abdcde9
Mike Powell <mike@lakeslegendaries.com>
2022-03-10 12:04:54 -0500

    compiling pages

--- script/loading.js
+++ script/loading.js
@@ -28,3 +28,9 @@ $('#video_cover').click(function(){
     document.getElementById("landing_video").style.display = "block";
     document.getElementById("landing_video").play();
 })
+
+// see button thumbnails
+$('#video_button').click(function(){
+    document.getElementById("video_button").classList.add("clicked");
+    document.getElementById("video_thumbnails").style.display = "block";
+})
