83aeb8713bb4ede25bf3b8f6d9a670daed7603ed
Mike Powell <mike@lakeslegendaries.com>
2022-03-10 11:11:05 -0500

    streamlining code

--- script/loading.js
+++ script/loading.js
@@ -2,29 +2,29 @@
 document.getElementById('title_video').addEventListener(
     'ended',
     function() {
-
+        
         // preserve body height
         const body_height = document.getElementById('title_video').clientHeight + "px";
-        for (const element of ['body', 'landing_cover']) {
+        for (const element of ['body', 'video_cover']) {
             document.getElementById(element).style.height = body_height;
         }
-        
+
         // fade-out loading screen
-        $("#loading_screen").fadeOut(1000);
+        $("#title_video").fadeOut(1000);
 
         // fade-in header/footer
         $("#header").fadeTo(1000, 1);
         $("#footer").fadeTo(1000, 1);
 
         // make video cover image visible
-        document.getElementById("video_cover").style.opacity = 1;
+        document.getElementById("video_cover").style.display = "block";
     },
     false
 );
 
 // click cover to play video
-$('#landing_cover').click(function(){
-    $('#landing_cover').fadeOut(1);
-    document.getElementById("main_video").style.opacity = 1;
+$('#video_cover').click(function(){
+    $('#video_cover').fadeOut(1);
+    document.getElementById("landing_video").style.display = "block";
     document.getElementById("landing_video").play();
 })
