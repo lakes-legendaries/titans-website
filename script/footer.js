035fc90ad07477ff3ce1049dcdd0ec3aa95d7b77
Mike Powell <mike@lakeslegendaries.com>
2022-03-10 16:51:15 -0500

    thumbnails for reading/pnp

--- script/footer.js
+++ script/footer.js
@@ -1,5 +1,26 @@
-// see button thumbnails
-$('#video_button').click(function(){
-    document.getElementById("video_button").classList.add("clicked");
-    document.getElementById("video_thumbnails").style.display = "block";
-})
+// general function for toggling blocks
+function show_block(prefix) {
+    return function() {
+
+        // reset other blocks
+        for (var p of ["video", "read", "pnp"]) {
+
+            // de-highlight buttons
+            document.getElementById(p + "_button").classList.remove("clicked");
+
+            // hide thumbnails
+            document.getElementById(p + "_thumbnails").style.display = "none";
+        }
+        
+        // highlight button
+        document.getElementById(prefix + "_button").classList.add("clicked");
+
+        // show thumbnails
+        document.getElementById(prefix + "_thumbnails").style.display = "block";
+    }
+}
+
+// set jqueries
+for (var prefix of ["video", "read", "pnp"]) {
+    $("#" + prefix + "_button").click(show_block(prefix));
+}
